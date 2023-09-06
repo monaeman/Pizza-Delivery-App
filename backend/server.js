2d8fe664a6d9a1b080d18720c4b3ccb60dfb86c7
monaeman <mona.eman2018@gmail.com>
2023-09-06 19:11:12 -0400

    added a server that can get orders on post man

--- backend/server.js
+++ backend/server.js
@@ -4,4 +4,8 @@ const port = process.env.PORT || 5003
 
 const app = express()
 
+app.get('/api/orders',(req, res)=> {
+    res.send('Get orders')
+})
+
 app.listen(port, ()=> console.log(`Server started on port ${port}`))
