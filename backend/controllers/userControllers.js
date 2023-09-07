78ecc009b03e4e05f39996e8958b87d7fab18878
monaeman <mona.eman2018@gmail.com>
2023-09-07 15:24:38 -0400

    user able to register  with name email and password

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -13,8 +13,34 @@ const registerUser =asyncHandler(async(req, res) => {
     }
 
     //check if user exists
-    
-    res.json({message: ' Register User'})
+    const userExists = await User.findOne({email})
+    if(userExists) {
+        res.status(400)
+        throw new Error('User already exists')
+    }
+    // Hash password
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    //Create user
+    const user = await User.create({
+        name,
+        email,
+        password: hashedPassword
+    })
+
+    if(user){
+        res.status(201).json({
+            _id: user.id,
+            name: user.name,
+            email: user.email
+        })
+    } else {
+        res.status(400)
+        throw new Error ('Invalid user data')
+    }
+
+   
 })
 
 
