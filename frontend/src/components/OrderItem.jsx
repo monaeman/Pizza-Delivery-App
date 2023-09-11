d9ab194b5f42e1c4e8c952c2ee97c96672a0c41d
monaeman <mona.eman2018@gmail.com>
2023-09-11 10:53:59 -0400

    added loaded and loading function

--- frontend/src/components/OrderItem.jsx
+++ frontend/src/components/OrderItem.jsx
@@ -1,20 +1,29 @@
-import { useDispatch } from 'react-redux'
-import { deleteOrder } from '../features/orders/orderSlice'
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { deleteOrder } from '../features/orders/orderSlice';
 
 function OrderItem({ order }) {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
-  return (
-    <div className='order'>
-      {/* <div>{new Date(order.createdAt).toLocaleString('en-US')}</div> */}
-      <h2>{order.name}</h2>
-      <h2>{order.varients}</h2>
-      <h2>{"$" + order.prices}</h2>
-      <button onClick={() => dispatch(deleteOrder(order._id))} className='close'>
-        X
-      </button>
-    </div>
-  )
+  const loaded = () => {
+    return (
+      <div className='order'>
+        <h2>{order.name}</h2>
+        <h2>{order.variants}</h2>
+        <h2>{"$" + order.prices}</h2>
+        
+        <button onClick={() => dispatch(deleteOrder(order._id))} className='close'>
+          X
+        </button>
+      </div>
+    );
+  }
+
+  const loading = () => {
+    return <h1>No order to display</h1>;
+  }
+
+  return order ? loaded() : loading();
 }
 
-export default OrderItem
+export default OrderItem;
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,61 +1,62 @@
-import {useEffect} from 'react'
-import {useNavigate} from 'react-router-dom'
-import {useSelector, useDispatch} from 'react-redux'
-import OrderForm from '../components/OrderForm'
-import Spinner from '../components/Spinner'
-import {getOrders, reset} from '../features/orders/orderSlice'
-import OrderItem from '../components/OrderItem'
-
-
-
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import OrderForm from "../components/OrderForm";
+import { getOrders, reset } from "../features/orders/orderSlice";
+import OrderItem from "../components/OrderItem";
 
 function Dashboard() {
-  const navigate = useNavigate()
-  const dispatch = useDispatch()
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-  const {user} = useSelector((state)=> state.auth)
-  const {orders, isLoading, isError } = useSelector((state) => state.orders)
-  
-  
-  
-  useEffect(()=> {
-    if(isError){
-      //console.log(message);
+  const { user } = useSelector((state) => state.auth);
+  const { orders, isError, message } = useSelector(
+    (state) => state.orders
+  );
+
+  useEffect(() => {
+    if (isError) {
+      // Handle error
+      // console.log(message);
     }
-    if(!user){
-      navigate('/login')
-    }else{
-    dispatch(getOrders())
+    if (!user) {
+      navigate("/login");
+    } else {
+      dispatch(getOrders());
+    }
+    return () => {
+      dispatch(reset());
+    };
+  }, [user, navigate, isError, dispatch, message]);
+
+  const loaded = () => {
+    return (
+      <>
+        <section className="heading">
+          <h1>Welcome {user && user.name}</h1>
+          <p>Order your Pizza</p>
+        </section>
+        <OrderForm />
+        <section className="content">
+          {orders.length > 0 ? (
+            <div className="orders">
+              {orders.map((order) => (
+                <OrderItem key={order._id} order={order} />
+              ))}
+            </div>
+          ) : (
+            <h3>You have not set any orders</h3>
+          )}
+        </section>
+      </>
+    );
   };
-    return() => {
-      dispatch(reset())
-    }
-  }, [user, navigate, isError,  dispatch])
 
+  const loading = () => {
+    return <h3>You have not set any orders</h3>;
+  };
 
-if(isLoading){
-  return <Spinner />
-}
-console.log(orders);
-  return ( <>
-    <section className="heading">
-<h1>Welcome {user && user.name}</h1>
-<p>Order your Pizza</p>
-</section>
-<OrderForm />
-<section className="content">
-{orders.length > 0 ? (
-  <div className='orders'>
-    {orders.map((order) => (
-      <OrderItem  key={order._id} order={order}/>
-    ))}
-  </div>
-) : ( 
-<h3> you have not set any orders </h3>)}
-    
- </section>
-  </>
-  )
+  return orders ?  loaded() : loading() ;
 }
 
-export default Dashboard
+export default Dashboard;
